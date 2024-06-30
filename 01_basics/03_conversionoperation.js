80483e7e70f094367c419f12e43d3148f8093eaa
techfusion3 <151348902+techfusion3@users.noreply.github.com>
2024-06-30 04:26:04 +0000

    conversion operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -20,5 +20,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let somenumber = 33
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************** Operations **********************************
+
+let value = 3
+let negvalue = -value 
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(3%2)
+
+let str1 = "hello"
+let str2 = " Faijan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + (2 + 2));
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let x = 3;
+const y = ++x;
+console.log(`x: ${x}, y: ${y}`);
+
+let a = 3;
+const b = a++;
+console.log(`a: ${a}, b: ${b}`);
+
+
+
+
