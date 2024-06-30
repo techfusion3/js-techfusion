ad12faa00618b0fb62a5015dffbc33a67548bec5
techfusion3 <151348902+techfusion3@users.noreply.github.com>
2024-06-30 10:35:08 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,27 @@
+// Primitive
+
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 376353725246426383637463839n
+
+const heroes = ["Iron Man", "Mahesh Babu", "Shah Rukh Khan"];
+let myObj = {
+    name: "Faijan",
+    age : 22,
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof heroes);
